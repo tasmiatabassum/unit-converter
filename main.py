e4b5c4023bdab8508b95c8d692c58c6d6cd7a5f3
tasmiatabassum <70054690+tasmiatabassum@users.noreply.github.com>
2021-05-01 21:15:38 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+def print_menu():
+    print('1.   Meter to Kilometer')
+    print('2.   Kilometer to Meter')
+    print('3.   Meter to Centimert')
+    print('4.   Centimeter to Meter')
+    print('5.   Meter to Millimeter')
+    print('6.   Millimeter to Meter')
+    print('7.   Meter to Macrometer')
+    print('8.   Macrometer to Meter')
+    print('9.   Meter to Nanometer')
+    print('10.  Nanometer to Meter')
+
+
+print_menu()
